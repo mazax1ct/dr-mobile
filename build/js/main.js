e049f4dac152d5eccc968d96fa9f850631ce7a1c
mazax1ct <vova@giveandget.ru>
2019-12-27 17:32:31 +0300

    popups

--- build/js/main.js
+++ build/js/main.js
@@ -17,7 +17,63 @@ $(document).on('click', '.js-subsection-opener', function () {
 $(".js-performance-type a").click(function () {
   $(".js-performance-type a").removeClass("is-active");
   $(this).addClass("is-active");
-  $(".bars__tab").removeClass("is-active");
-  $(".bars__tab[data-target=" + $(this).attr("data-target") + "]").addClass("is-active");
+  $(".bars__tab").removeClass("is-open");
+  $(".bars__tab[data-target=" + $(this).attr("data-target") + "]").addClass("is-open");
+  return false;
+});
+
+//переключение brige
+$(document).on('click', '.js-brige-button', function () {
+  $(this).parent('.brige').find('.js-brige-button').removeClass('is-active');
+  $(this).addClass('is-active');
+  return false;
+});
+
+//переключение табов в подсекции
+$(".js-tabs a").click(function () {
+  var parent = $(this).closest(".subsection__inner");
+  $(".js-tabs a").removeClass("is-active");
+  $(this).addClass("is-active");
+  parent.find(".tab").removeClass("is-open");
+  $(".tab[data-target=" + $(this).attr("data-target") + "]").addClass("is-open");
+  return false;
+});
+
+var back = document.querySelector('.js-back');
+
+function popupClose() {
+  $('body').removeClass('overflow');
+  $('.popup').removeClass('is-open');
+  $('.footer').removeClass('footer--config_open');
+  back.removeEventListener('click', popupClose);
+}
+
+//окрытие попапа с описанием
+$(document).on('click', '.js-more', function () {
+  $('body').addClass('overflow');
+  $('#'+ $(this).attr('data-target')).addClass('is-open');
+  back.addEventListener('click', popupClose);
+  return false;
+});
+
+//сохранение конфигурации
+$(document).on('click', '.js-save', function () {
+  $('body').addClass('overflow');
+  $('#config').addClass('is-open');
+  $('.footer').addClass('footer--config_open');
+  back.addEventListener('click', popupClose);
+  return false;
+});
+
+//закрытие попапа
+$(document).on('click', '.js-popup-close', function () {
+  popupClose();
+  return false;
+});
+
+$(document).on('click', '.js-graphs', function () {
+  $('body').addClass('overflow');
+  $('#graphs').addClass('is-open');
+  back.addEventListener('click', popupClose);
   return false;
 });
